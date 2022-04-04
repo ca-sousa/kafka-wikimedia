0d381b14ff8ebbf493913589ded226c5335ed926
Carla Sousa <sousa.carla09@gmail.com>
2022-04-04 17:39:24 -0300

    Commit offset manually

--- src/main/java/kafka/openSearch/openSearchConsumer.java
+++ src/main/java/kafka/openSearch/openSearchConsumer.java
@@ -70,6 +70,7 @@ public class openSearchConsumer {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         // create consumer
         return new KafkaConsumer<>(properties);
@@ -130,6 +131,10 @@ public class openSearchConsumer {
                         log.error(e.toString());
                     }
                 }
+
+                // commit offsets
+                consumer.commitSync();
+                log.info("Offsets have been Commited");
             }
         }
 
