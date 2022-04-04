aa6461362bdfe6b0e6895f5d4244b58446df50e4
Carla Sousa <sousa.carla09@gmail.com>
2022-04-04 16:06:40 -0300

    OpenSearch configuration

--- src/main/java/kafka/openSearch/openSearchConsumer.java
+++ src/main/java/kafka/openSearch/openSearchConsumer.java
@@ -1,8 +1,80 @@
 package kafka.openSearch;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.opensearch.client.OpenSearchClient;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+
 public class openSearchConsumer {
 
-    public static void main(String[] args) {
+    public static RestHighLevelClient createOpenSearchClient() {
+
+        String connStirng = "http://localhost:9200";
+
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connStirng);
+
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme())));
+        } else {
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                            ));
+        }
+        return restHighLevelClient;
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        Logger log = LoggerFactory.getLogger(openSearchConsumer.class.getSimpleName());
+
+        // create OpenSearch Client
+        RestHighLevelClient openSearchClient = createOpenSearchClient();
+
+        // create the index on OpenSearch
+        try (openSearchClient) {
+
+            boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
+
+            if (!indexExists) {
+                CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
+                openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                log.info("Wikimedia index has been created");
+            } else {
+                log.info("Wikimedia index already exists");
+            }
+
+        }
+
+        // create Kafka Client
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+        // close connections
 
     }
 }
